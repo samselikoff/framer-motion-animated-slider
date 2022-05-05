b6c00bcbfe2f2946c7a8079918495272e3275338
Sam Selikoff <sam.selikoff@gmail.com>
2022-05-05 13:04:44 -0400

    works with scaling and safari, but useEffect

--- src/App.js
+++ src/App.js
@@ -41,42 +41,17 @@ export default function App() {
       </div>
     </div>
   );
-
-  // let [progress, setProgress] = useState(100);
-  // let min = 0;
-  // let max = 400;
-
-  // return (
-  //   <div className="p-2 bg-blue-100 h-[400px] overflow-y-scroll">
-  //     <div className="w-full px-8 py-8">
-  //       <div className="flex justify-between mb-4">
-  //         <span className="border-l border-green-500">{min}</span>
-  //         <span className="border-r border-green-500">{max}</span>
-  //       </div>
-  //       <Slider min={min} max={max} value={progress} onChange={setProgress} />
-  //       <div className="mt-8">
-  //         <p>Current: {Math.floor(progress * 10) / 10}</p>
-  //       </div>
-  //       <div>
-  //         <button onClick={() => setProgress(50)}>Set to 50</button>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 }
 
 function Slider({ min, max, value, onChange }) {
   let handleRef = useRef();
   let constraintsRef = useRef();
-  let handleSize = 30;
+  let progressBarRef = useRef();
+  let handleSize = 20;
   let scaledHandleSize = 30;
   let handleX = useMotionValue(-(scaledHandleSize - handleSize) / 2);
   let dragControls = useDragControls();
-  let fullBarRef = useRef();
-  let fullBarBounds = useBounds(fullBarRef);
-  let fullBarWidth = fullBarBounds?.width;
-  let progressWidth = useTransform(handleX, (v) => v + handleSize);
-  let progressBarRef = useRef();
+  let progressWidth = useTransform(handleX, (v) => v + scaledHandleSize / 2);
 
   function handleDrag() {
     let handleBounds = handleRef.current.getBoundingClientRect();
@@ -88,61 +63,58 @@ function Slider({ min, max, value, onChange }) {
     onChange(newProgress * (max - min));
   }
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     let newProgress = value / (max - min);
     let progressBarBounds = progressBarRef.current.getBoundingClientRect();
     let middleOfHandle = newProgress * progressBarBounds.width;
+    let newX = middleOfHandle - (scaledHandleSize - handleSize) / 2;
 
-    handleX.set(middleOfHandle - (scaledHandleSize - handleSize) / 2);
+    handleX.set(newX);
   }, [handleSize, handleX, max, min, scaledHandleSize, value]);
 
   return (
-    <div data-test="slider" className="relative flex items-center">
+    <div
+      data-test="slider"
+      className="relative flex items-center"
+      style={{ height: handleSize }}
+    >
       <div
         data-test="slider-constraints"
         ref={constraintsRef}
-        className=" absolute h-0.5 rounded-full"
-        // style={{ left: -handleSize / 2, right: -handleSize / 2 }}
-        // style={{
-        //   left: 0,
-        //   right: 0,
-        // }}
+        className="absolute h-0.5 rounded-full"
         style={{
           left: -((scaledHandleSize - handleSize) / 2),
           right: -((scaledHandleSize - handleSize) / 2),
         }}
       />
 
-      <div
-        className="absolute inset-x-0 flex items-center"
-        // style={{ left: handleSize / 2, right: handleSize / 2 }}
-      >
+      <div className="absolute inset-x-0 flex items-center">
         <div
           data-test="slider-background"
-          ref={fullBarRef}
           className="absolute inset-x-0 flex items-center py-1 cursor-grab"
           onPointerDown={(event) => {
-            dragControls.start(event, { snapToCursor: true });
-            let { left, width } = fullBarRef.current.getBoundingClientRect();
+            let { left, width } =
+              progressBarRef.current.getBoundingClientRect();
             let position = event.pageX - left;
             let newProgress = position / width;
-            onChange(newProgress * (max - min));
+            let newValue = newProgress * (max - min);
+            onChange(clamp(newValue, min, max));
           }}
         >
           <div className="w-full h-0.5 bg-gray-300 rounded-full" />
         </div>
 
-        {/* <motion.div
-          data-test="slider-current-progress"
+        <motion.div
+          data-test="slider-filled-background"
           className="absolute h-0.5 rounded-full bg-gray-700 pointer-events-none"
-          style={{ left: 0, width: progressWidth }}
-        /> */}
+          style={{ width: progressWidth }}
+        />
       </div>
 
       <div
         data-test="slider-progress"
         ref={progressBarRef}
-        className="absolute h-1 bg-blue-500"
+        className="absolute pointer-events-none"
         style={{ left: handleSize / 2, right: handleSize / 2 }}
       ></div>
 
@@ -152,13 +124,12 @@ function Slider({ min, max, value, onChange }) {
         drag="x"
         whileDrag={{ scale: 1 }}
         dragConstraints={constraintsRef}
-        // dragConstraints={{ left: -handleSize / 2 }}
         dragControls={dragControls}
         dragElastic={0}
         dragMomentum={false}
         onDrag={handleDrag}
         transition={{ type: "tween", duration: 0.15 }}
-        className="relative bg-red-500 rounded-full opacity-50 cursor-grab active:cursor-grabbing"
+        className="absolute bg-red-500 rounded-full opacity-50 cursor-grab active:cursor-grabbing"
         style={{
           x: handleX,
           width: scaledHandleSize,
@@ -172,4 +143,6 @@ function Slider({ min, max, value, onChange }) {
   );
 }
 
-function getProgressFromX({ x, containerRef }) {}
+function clamp(number, min, max) {
+  return Math.max(min, Math.min(number, max));
+}
